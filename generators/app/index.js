d46bf4963d883a2e00ab0971788c0573187acaee
Matt Dean <matt@trabian.com>
2015-05-25 22:25:35 -0400

    Use this.log instead of console.warn in default generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,7 +2,7 @@ var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
   warn: function() {
-    console.warn('The default generator for react-generator is currently empty. Check out the subgenerators.');
+    this.log('The default generator for react-generator is currently empty. Check out the subgenerators.');
     this.config.save();
   }
 });
